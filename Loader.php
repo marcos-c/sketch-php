8ae7f70db78aeb18b5c9389bce92fa84425bb61f
Marcos Cooper <marcos@feimstudio.com>
2011-09-08 10:52:30 +0900

    Renamed Stub for Loader, updated to use addRole instead of add Rule, added better error support when returning errors from JSON calls, and by adding the context name to the exception. Also added a send-exceptions-from context parameter.

--- Loader.php
+++ Loader.php
@@ -3,7 +3,7 @@
  * This file is part of the Sketch Framework
  * (http://code.google.com/p/sketch-framework/)
  *
- * Copyright (C) 2010 Marcos Albaladejo Cooper
+ * Copyright (C) 2011 Marcos Albaladejo Cooper
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -22,19 +22,6 @@
  * @package Sketch
  */
 
-if (!defined('APPLICATION_PATH')) {
-    define('APPLICATION_PATH', realpath(dirname(__FILE__).'/../../'));
-}
-set_include_path(realpath(APPLICATION_PATH.'/library').PATH_SEPARATOR.get_include_path());
-
-if (!defined('CONTEXT_XML')) {
-    define('CONTEXT_XML', APPLICATION_PATH.'/config/context.xml');
-}
-
-if (!defined('DETACH_CONTROLLER')) {
-    define('DETACH_CONTROLLER', false);
-}
-
 require_once 'Sketch/Application.php';
 require_once 'Sketch/Resource/Factory.php';
 require_once 'Sketch/Resource/Context.php';
@@ -49,7 +36,6 @@ require_once 'Sketch/Response/Exception.php';
 require_once 'Sketch/Session.php';
 require_once 'Sketch/Form.php';
 require_once 'Sketch/DateTime.php';
-require_once 'Sketch/DateTime/Iterator.php';
 require_once 'Sketch/Mail/Message.php';
 require_once 'Sketch/Mail/Transport.php';
 
@@ -60,14 +46,16 @@ try {
     set_error_handler(array('SketchApplication', 'exceptionErrorHandler'));
     $application->setStartTime(microtime(true));
     $application->setDocumentRoot(APPLICATION_PATH);
-    $application->setContext(SketchResourceFactory::getContext(CONTEXT_XML));
+    $application->setContext(
+        SketchResourceFactory::getContext(defined('CONTEXT_XML') ? CONTEXT_XML : APPLICATION_PATH.'/config/context.xml')
+    );
     // Initialize request
     $application->setRequest(new SketchRequest());
     // Initialize session and ACL
     $application->setSession(new SketchSession());
     if (!($application->getSession()->getACL() instanceof SketchSessionACL)) {
         $acl = new SketchSessionACL();
-        $acl->addRule('guest');
+        $acl->addRole('guest');
         $application->getSession()->setACL($acl);
     }
     // Initialize default locale
@@ -82,28 +70,29 @@ try {
     $application->setConnection(
         SketchResourceFactory::getConnection($application->getContext())
     );
-    if (!DETACH_CONTROLLER) {
-        // Initialize controller
-        $application->setController(new SketchController());
-        $application->getController()->setRouter(
-            SketchRouterFactory::getRouter($application->getRequest())
-        );
-        // Output response
-        if ($application->getRequest()->isJSON()) {
-            $application->getController()->setResponse(SketchResponseJSON::HTML());
-            print SketchUtils::encodeJSON($application->getController()->getResponse());
-        } else {
-            $application->getController()->setResponse(SketchResponse::HTML());
-            $application->getController()->setResponseFilters($application->getContext());
-            print $application->getController()->getResponse();
-        }
+    // Initialize controller
+    $application->setController(new SketchController());
+    $application->getController()->setRouter(
+        SketchRouterFactory::getRouter($application->getRequest())
+    );
+    // Output response
+    if ($application->getRequest()->isJSON()) {
+        $application->getController()->setResponse(new SketchResponseJSON());
+        print SketchUtils::encodeJSON($application->getController()->getResponse());
+    } else {
+        $application->getController()->setResponse(new SketchResponse());
+        $application->getController()->setResponseFilters($application->getContext());
+        print $application->getController()->getResponse();
     }
 } catch (Exception $e) {
-    // Can't rely on $_SERVER['DOCUMENT_ROOT'] because it doesn't return what you would
-    // expect on all situations (symbolic links, server configuration, etc.)
+    /**
+     * Can't rely on $_SERVER['DOCUMENT_ROOT'] because it doesn't return what you would expect on all situations
+     * (symbolic links, server configuration, etc.)
+     */
     $server_document_root = str_replace($_SERVER['SCRIPT_NAME'], '', realpath(basename($_SERVER['SCRIPT_NAME'])));
     $file_name = str_replace($server_document_root, '', $e->getFile());
-    if ($application->getContext()->getLayerName() != 'production') {
+    if ($application->getContext()->getLayerName() == 'development') {
+        ob_start();
         print '<pre>';
         print "<b>You have an exception!</b>\n".trim($e->getMessage())."\n<i>Thrown on line ".$e->getLine()." ($file_name)</i>\n";
         print "<b>Trace</b>\n";
@@ -124,12 +113,18 @@ try {
             }
             print "</pre>";
         }
+        if ($application->getRequest()->isJSON()) {
+            print SketchUtils::encodeJSON(array('html' => ob_get_clean()));
+        } else {
+            print ob_get_clean();
+        }
     } else {
-        $parameters = $application->getContext()->getParametersFor('library', 'Stub.php');
-        if ($parameters['send-exceptions-to']['email-address'] != null) {
+        $parameters = $application->getContext()->getParametersFor('library', 'Loader.php');
+        $name = $application->getContext()->getName();
+        if ($parameters['send-exceptions-from']['email-address'] != null && $parameters['send-exceptions-to']['email-address'] != null) {
             ob_start();
             print '<pre>';
-            print "<b>You have an exception!</b>\n".trim($e->getMessage())."\n<i>Thrown on line ".$e->getLine()." ($file_name)</i>\n";
+            print "<b>An exception was thrown in $name!</b>\n".trim($e->getMessage())."\n<i>Thrown on line ".$e->getLine()." ($file_name)</i>\n";
             print "<b>Trace</b>\n";
             foreach ($e->getTrace() as $r) {
                 if (array_key_exists('class', $r) && $r['function'] != 'exceptionErrorHandler') {
@@ -149,10 +144,11 @@ try {
                 print "</pre>";
             }
             $message = new SketchMailMessage();
-            $reply_to = new SketchMailAddress($parameters['send-exceptions-to']['email-address']);
+            $reply_to = new SketchMailAddress($parameters['send-exceptions-from']['email-address']);
             $message->setReplyTo($reply_to);
             $message->setFrom($reply_to);
-            $message->addRecipient(MESSAGE_TO, $reply_to);
+            $to = new SketchMailAddress($parameters['send-exceptions-to']['email-address']);
+            $message->addRecipient(MESSAGE_TO, $to);
             if ($parameters['send-exceptions-to']['email-address-2'] != null) {
                 $message->addRecipient(MESSAGE_CC, new SketchMailAddress($parameters['send-exceptions-to']['email-address-2']));
             }
@@ -165,11 +161,15 @@ try {
             if ($parameters['send-exceptions-to']['email-address-5'] != null) {
                 $message->addRecipient(MESSAGE_CC, new SketchMailAddress($parameters['send-exceptions-to']['email-address-5']));
             }
-            $message->setSubject('Exception in Sketch');
+            $message->setSubject("An exception was thrown in $name!");
             $message->setContent(ob_get_clean());
             SketchMailTransport::sendMessage($message);
         }
-        print "An error has occurred, please try again later.";
+        if ($application->getRequest()->isJSON()) {
+            print SketchUtils::encodeJSON(array('html' => "An error has occurred, please try again later."));
+        } else {
+            print "An error has occurred, please try again later.";
+        }
     }
 }
 exit();
