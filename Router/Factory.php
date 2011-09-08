ba93a90fabb2f09b3a0a8a185ff5a89187f9aba0
Marcos Cooper <marcos@feimstudio.com>
2011-09-08 11:12:00 +0900

    Updated SketchRouterFactory documentation.

--- Router/Factory.php
+++ Router/Factory.php
@@ -3,7 +3,7 @@
  * This file is part of the Sketch Framework
  * (http://code.google.com/p/sketch-framework/)
  *
- * Copyright (C) 2010 Marcos Albaladejo Cooper
+ * Copyright (C) 2011 Marcos Albaladejo Cooper
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -28,14 +28,14 @@ require_once 'Sketch/Router/Rewrite.php';
 
 /**
  * SketchRouterFactory
- *
- * @package Sketch
  */
 class SketchRouterFactory extends SketchObject {
     /**
+     * Rreturn the current router
      *
+     * @static
      * @param SketchRequest $request
-     * @return SketchRouter
+     * @return SketchRouterDefault|SketchRouterRewrite
      */
     static function getRouter(SketchRequest $request) {
         if ($request->isRedirect()) {
