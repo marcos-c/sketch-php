c7caaec6277dc09f23d494d3b8f386fd1ebab757
Marcos Cooper <marcos.ac@gmail.com>
2013-04-04 14:07:52 +0900

    Adds a Twitter Bootstrap based pager component.

--- library/Sketch/FormList.php
+++ library/Sketch/FormList.php
@@ -136,4 +136,94 @@ class FormList extends FormView {
             $o[$i] = ($i == $offset) ? '[ '.($i + 1).'-'.($j + 1).' ]' : '[ '.$this->commandLink(new FormCommand('offset', $i), null, ($i + 1).'-'.($j + 1)).' ]';
         } return implode(' ', $o);
     }
+
+    function getCurrentPage($size) {
+        $limit = $this->getInstance()->getLimit();
+        $current_offset = $this->getInstance()->getOffset();
+        if ($limit > 0) {
+            $last_page = ceil($size / $limit);
+            $current_page = intval($current_offset / $limit) + 1;
+        } else {
+            $last_page = 1;
+            $current_page = 1;
+        }
+        if ($current_page > 5 && $current_page < ($last_page - 5)) {
+            $from = $current_page - 5;
+            $to = $current_page + 5;
+        } else if ($current_page <= 5) {
+            $from = 1;
+            $to = (($to = $from + 9) < $last_page) ? $to : $last_page;
+        } else if ($current_page >= ($last_page - 5)) {
+            $to = $last_page;
+            $from = (($from = $to - 9) > 1) ? $from : 1;
+        }
+        $translator = $this->getTranslator();
+        return sprintf($translator->_('Página %d de %d'), $current_page, $last_page);
+    }
+
+    function getBootstrapPager($size, $parameters = null) {
+        $limit = $this->getInstance()->getLimit();
+        $current_offset = $this->getInstance()->getOffset();
+        if ($limit > 0) {
+            $last_page = ceil($size / $limit);
+            $current_page = intval($current_offset / $limit) + 1;
+        } else {
+            $last_page = 1;
+            $current_page = 1;
+        }
+        if ($current_page > 5 && $current_page < ($last_page - 5)) {
+            $from = $current_page - 5;
+            $to = $current_page + 5;
+        } else if ($current_page <= 5) {
+            $from = 1;
+            $to = (($to = $from + 9) < $last_page) ? $to : $last_page;
+        } else if ($current_page >= ($last_page - 5)) {
+            $to = $last_page;
+            $from = (($from = $to - 9) > 1) ? $from : 1;
+        }
+        $translator = $this->getTranslator();
+        $parameters = $this->extend(array(
+            'show' => 'first,previous,pages,next,last',
+            'page_size_label' => $translator->_('showing %s per page'),
+            'first-label' => $translator->_('First'),
+            'previous-label' => $translator->_('Previous'),
+            'next-label' => $translator->_('Next'),
+            'last-label' => $translator->_('Next'),
+            'selected' => array('id' => null, 'class' => 'active', 'style' => null),
+            'pager' => array('id' => null, 'class' => 'pager', 'style' => null),
+            'first' => array('id' => null, 'class' => 'first', 'style' => null),
+            'previous' => array('id' => null, 'class' => 'previous', 'style' => null),
+            'next' => array('id' => null, 'class' => 'next', 'style' => null),
+            'last' => array('id' => null, 'class' => 'last', 'style' => null),
+            'size' => array('id' => null, 'class' => 'size', 'style' => null)
+        ), $parameters);
+        $show = array_map('trim', explode(',', $parameters['show']));
+        $current = in_array('current', $show) ? '<li>'.intval($current_offset + 1).' - '.(($current_page == $last_page) ? $size : intval($current_offset + $limit)).'</li>' : '';
+        $of_rows = in_array('of_rows', $show) ? '<li'.$parameters['rows'].'>'.sprintf($translator->_('of %s'), $size).'</li>' : '';
+        $pages = array();
+        for ($i = $from; $i <= $to; $i++) {
+            $pages[] = (($offset = ($i - 1) * $limit) == $current_offset) ? '<li'.$parameters['selected'].'><span>'.$i.'</span></li>' : '<li>'.$this->commandLink(new FormCommand('offset', $offset), null, $i).'</li>';
+        }
+        $of_pages = (in_array('of_pages', $show)) ? "<li".$parameters['size']."\">of $last_page</li>" : '';
+        $first = (in_array('first', $show) && $from > 1) ? '<li'.$parameters['first'].'>'.$this->commandLink(new FormCommand('offset', 0), null, $parameters['first-label']).'</li>' : '';
+        $previous = (in_array('previous', $show) && $current_page > 1) ? '<li'.$parameters['previous'].'>'.$this->commandLink(new FormCommand('offset', $current_offset - $limit), null, $parameters['previous-label']).'</li>' : '';
+        $next = (in_array('next', $show) !== false && $current_page < $last_page) ? '<li'.$parameters['next'].'>'.$this->commandLink(new FormCommand('offset', $current_offset + $limit), null, $parameters['next-label']).'</li>' : '';
+        $last = (in_array('last', $show) && $last_page > 10 && $current_page < ($last_page - 5)) ? '<li'.$parameters['last'].'>'.$this->commandLink(new FormCommand('offset', ($last_page - 1) * $limit), null, $parameters['last-label']).'</li>' : '';
+        $page_size = (in_array('page_size', $show) && $size > 10) ? '<li'.$parameters['page_size'].'>'.sprintf($parameters['page_size_label'], $this->selectOne(array(10 => 10, 25 => 25, 50 => 50), 'limit', 'onchange="'.$this->command(new FormCommandPropagate(), null).'"')).'</li>' : '';
+        $output = array();
+        foreach ($show as $key) {
+            switch ($key) {
+                case 'current': $output[$key] = $current; break;
+                case 'of_rows': $output[$key] = $of_rows; break;
+                case 'pages': $output[$key] = implode(' ', $pages); break;
+                case 'of_pages': $output[$key] = $of_pages; break;
+                case 'first': $output[$key] = $first; break;
+                case 'previous': $output[$key] = $previous; break;
+                case 'next': $output[$key] = $next; break;
+                case 'last': $output[$key] = $last; break;
+                case 'page_size': $output[$key] = $page_size; break;
+            }
+        }
+        return '<ul'.$parameters['pager'].'>'.trim(implode(' ', $output)).'</ul>';
+    }
 }
