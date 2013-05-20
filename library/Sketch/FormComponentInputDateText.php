db7cc21ad7f9b1eca5108f897d8392863ba96728
Marcos Cooper <marcos.ac@gmail.com>
2013-05-20 10:17:04 +0900

    Adds a new input date text form component.

---
+++ library/Sketch/FormComponentInputDateText.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * This file is part of the Sketch library
+ *
+ * @author Marcos Cooper <marcos@releasepad.com>
+ * @version 2.0.12
+ * @copyright 2007 Marcos Cooper
+ * @link http://releasepad.com/sketch
+ * @package Sketch
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, you can get a copy from the
+ * following link: http://opensource.org/licenses/lgpl-2.1.php
+ */
+
+namespace Sketch;
+
+class FormComponentInputDateText extends FormComponent {
+    function saveHTML() {
+        $arguments = $this->getArguments();
+        $attribute = array_shift($arguments);
+        $parameters = array_shift($arguments);
+        $field_name = $this->getForm()->getFieldName($attribute);
+        $field_value = $this->getLocale()->getFormatter()->formatDate($this->getForm()->getFieldValue($attribute));
+        $parameters = (($parameters != null && strpos(" $parameters", 'class="')) ? $parameters : implode(' ', array($parameters, 'class="input-text"')));
+        $parameters = (($parameters != null && strpos(" $parameters", 'id="'))) ? $parameters : implode(' ', array($parameters, 'id="'.$field_name.'"'));
+        return '<input type="text" name="'.$field_name.'" value="'.$field_value.'" '.$parameters.' />';
+    }
+}
