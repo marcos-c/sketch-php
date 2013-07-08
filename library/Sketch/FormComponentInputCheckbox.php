923e69ba6287f4e9e00a362fa4ebf91d1d86ec91
Marcos Cooper <marcos.ac@gmail.com>
2013-07-08 18:49:11 +0900

    Restores the hidden field in the inputCheckbox.

--- library/Sketch/FormComponentInputCheckbox.php
+++ library/Sketch/FormComponentInputCheckbox.php
@@ -36,6 +36,6 @@ class FormComponentInputCheckbox extends FormComponent {
         $parameters = (($parameters != null && strpos(" $parameters", 'class="')) ? $parameters : implode(' ', array($parameters, 'class="input-checkbox"')));
         $true = ($true != null) ? $true : 't';
         $check = is_bool($field_value) ? $field_value : ($field_value == $true);
-        return '<input type="checkbox" id="'.$field_name.'" name="'.$field_name.'" value="'.$true.'"'.($check ? ' checked="checked"' : '').' '.$parameters.' />';
+        return '<input type="hidden" name="'.$field_name.'" value="" /><input type="checkbox" id="'.$field_name.'" name="'.$field_name.'" value="'.$true.'"'.($check ? ' checked="checked"' : '').' '.$parameters.' />';
     }
 }
