3332692dc37ecbe8bdecdc6d53ddddc18b0e54ba
Marcos Cooper <marcos.ac@gmail.com>
2013-07-28 12:56:04 +0900

    Adds sami and phpunit to the project dependencies.

---
+++ sami.php
@@ -0,0 +1,8 @@
+<?php
+ 
+return new Sami\Sami('library', array(
+	'title' => 'Sketch API',
+	'build_dir' => __DIR__.'/public/api',
+	'cache_dir' => __DIR__.'/cache',
+	'default_opened_level' => 2
+));
