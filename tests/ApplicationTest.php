57faa717c5be36fda387c461a5ab2c816209317c
Marcos Cooper <marcos.ac@gmail.com>
2014-02-12 09:37:38 +0900

    Modifies the unit tests to take into account the new namespaces.

--- tests/ApplicationTest.php
+++ tests/ApplicationTest.php
@@ -4,12 +4,12 @@ require_once 'vendor/autoload.php';
 class ApplicationTest extends PHPUnit_Framework_TestCase {
     function testGetInstance() {
         $application = \Sketch\Application\Application::getInstance();
-        $this->assertInstanceOf('\Sketch\Application', $application);
+        $this->assertInstanceOf('\Sketch\Application\Application', $application);
     }
 
     function testStart() {
         $application = \Sketch\Application\Application::getInstance();
-        $application->load(dirname(__FILE__), true);
+        $application->load(dirname(__FILE__), true, true);
         $this->assertInstanceOf('\Sketch\ResourceContext', $application->getContext());
     }
 }
